18e61dddcda7eb17b964a122f7380c86aaf900ae
Felipe <felipefcor@gmail.com>
2020-04-07 10:09:16 +0200

    feat: not possible to add a title over 50 characters in the ad

--- src/main/java/domain/Ad/AdTitle.java
+++ src/main/java/domain/Ad/AdTitle.java
@@ -1,11 +1,13 @@
 package domain.Ad;
 
 import domain.Ad.DTO.AdDTOTitle;
+import domain.Ad.exceptions.TitleTooLongException;
 
 public class AdTitle {
     private String adTitle;
 
     public AdTitle(String adTitle) {
+        if(adTitle.length() > 50 ) throw new TitleTooLongException();
         this.adTitle = adTitle;
     }
 
---
+++ src/test/java/AdTitleShould.java
@@ -0,0 +1,13 @@
+import domain.Ad.AdTitle;
+import domain.Ad.exceptions.TitleTooLongException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;;
+
+public class AdTitleShould {
+    @Test
+    public void throw_an_exception_when_the_title_is_over_50_characters(){
+
+        Assertions.assertThrows(TitleTooLongException.class, () -> new AdTitle("Esto es una prueba para comprobar que no se puede añadir más de " +
+                "cincuenta caracteres a un titulo"));
+    }
+}
