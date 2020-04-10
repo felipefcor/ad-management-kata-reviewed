1893a879eaf010b7f9b75e62cd29f578f7554f7a
Felipe <felipefcor@gmail.com>
2020-04-10 11:22:07 +0200

    feat: datesorter and visitorsorter tests

---
+++ src/test/java/domain/DateSorterShould.java
@@ -0,0 +1,35 @@
+package domain;
+
+import domain.Ad.Ad;
+import domain.Ad.DateSorter;
+import domain.Ad.valueObjects.AdDescription;
+import domain.Ad.valueObjects.AdTitle;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DateSorterShould {
+    @Test
+    public void sort_a_list_of_ads_by_date(){
+        List<Ad> list = new ArrayList<>();
+        LocalDate dateTest3 = LocalDate.of(2020, 4, 3);
+        LocalDate dateTest2 = LocalDate.of(2020, 4, 2);
+        LocalDate dateTest4 = LocalDate.of(2020, 4, 4);
+        LocalDate dateTest = LocalDate.of(2020, 4, 1);
+        Ad ad = new Ad(new AdTitle("titulo"), new AdDescription("descripcion"), dateTest);
+        Ad ad2 = new Ad(new AdTitle("titulo2"), new AdDescription("descripcion2"), dateTest2);
+        Ad ad3 = new Ad(new AdTitle("titulo3"), new AdDescription("descripcion3"), dateTest3);
+        Ad ad4 = new Ad(new AdTitle("titulo4"), new AdDescription("descripcion4"), dateTest4);
+        list.add(ad4);
+        list.add(ad3);
+        list.add(ad);
+        list.add(ad2);
+
+        list.sort(new DateSorter());
+
+        Assert.assertEquals(ad, list.get(0));
+    }
+}
